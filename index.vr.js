706d9daad63be194fc7bdb3c1084a1c137da8ef7
Loris <loris4526@gmail.com>
2017-10-31 12:14:54 +0100

    Create 'Row' component that displays colors. Delete inline stylings, in favour of StyleSheet.create() object --> cleaner syntax

--- index.vr.js
+++ index.vr.js
@@ -1,89 +1,76 @@
-import React from 'react';
+//load React library in order to create components
+import React, {Component} from 'react'
+
+//required stuff in order to create ReactVR application
 import {
   AppRegistry,
   asset,
   Pano,
-  Text,
   View,
-} from 'react-vr';
+  Text,
+  StyleSheet
+} from 'react-vr'
 
 
-const textBox = {
-  fontSize: 0.8,
-  fontWeight: '400',
-  layoutOrigin: [0.5, 0.5],
-  paddingLeft: 0.2,
-  paddingRight: 0.2,
-  position: 'absolute',
-  textAlign: 'center'
-}
-const front = {
-  backgroundColor: 'red',
-  transform: [{translate: [0, 0, -3]}],
-}
-const back = {
-  backgroundColor: 'blue',
-  transform: [
-    {translate: [0, 0, 3]},
-    {rotateY: 180}
-  ],
-}
-const left = {
-  backgroundColor: 'green',
-  transform: [
-    {translate: [-3, 0, 0]},
-    {rotateY: 90}
-  ],
-}
-const right = {
-  backgroundColor: 'yellow',
-  transform: [
-    {translate: [3, 0, 0]},
-    {rotateY: -90}
-  ],
-}
-const top = {
-  backgroundColor: 'yellow',
-  transform: [
-    {translate: [0, 3, 0]},
-    {rotateX: 90}
-  ],
-}
-const bottom = {
-  backgroundColor: 'brown',
-  transform: [
-    {translate: [0, -3, 0]},
-    {rotateX: -90}
-  ],
-}
 
 
-export default class reactVR extends React.Component {
-  render() {
+
+class Row extends Component {
+
+  render(){
     return (
-      <View>
-        <Pano source={asset('chess-world.jpg')}/>
-        <Text style={Object.assign({},textBox,front)}>
-          Front
-        </Text>
-        <Text style={Object.assign({},textBox,back)}>
-          Back
-        </Text>
-        <Text style={Object.assign({},textBox,left)}>
-          Left
-        </Text>
-        <Text style={Object.assign({},textBox,right)}>
-          Right
-        </Text>
-        <Text style={Object.assign({},textBox,top)}>
-          Top
-        </Text>
-        <Text style={Object.assign({},textBox,bottom)}>
-          Bottom
+      <View
+        style={[
+          {backgroundColor: this.props.color},
+          styles.row
+        ]}>
+        <Text style={styles.text}>
+          {this.props.color}
         </Text>
       </View>
-    );
+    )
   }
-};
 
-AppRegistry.registerComponent('reactVR', () => reactVR);
+}
+
+
+
+export default class Basics extends Component {
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <Pano source={asset('room.jpg')}></Pano>
+
+        <Row color={'red'}/>
+        <Row color={'blue'}/>
+        <Row color={'green'}/>
+
+      </View>
+    )
+  }
+
+}
+
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 0.2,
+    textAlign: 'center'
+  },
+  row: {
+    width: 0.5,
+    height: 0.3,
+    margin: 0.1
+  },
+  container: {
+    flex: 1,
+    width: 2,
+    flexDirection: 'row',
+    transform: [
+      {translate: [-1,0,-3]}
+    ]
+  }
+})
+
+AppRegistry.registerComponent('Basics', ()=>Basics)
--- vr/client.js
+++ vr/client.js
@@ -6,7 +6,7 @@
 import {VRInstance} from 'react-vr-web';
 
 function init(bundle, parent, options) {
-  const vr = new VRInstance(bundle, 'reactVR', parent, {
+  const vr = new VRInstance(bundle, 'Basics', parent, {
     // Add custom options here
     enableHotReload: true,
     ...options,
